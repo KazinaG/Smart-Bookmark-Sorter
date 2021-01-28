1f04bdce82f7f85fde5be584fd3a7fb13cfe979e
kari <karrikari111@gmail.com>
2021-01-28 22:40:35 +0900

    Chrome起動時に監視処理を開始するよう修正,manifestのスクリプト読み込み順を変更

--- Automatic bookmark organization/js/trigger.js
+++ Automatic bookmark organization/js/trigger.js
@@ -1,10 +1,9 @@
-// ブックマークを移動した際に、ブックマーク移動確認→ソート（ブックマーク移動）→ブックマーク移動確認→…の無限ループが発生するため、それの抑止
-
+// 監視開始
+loop();
 
 //#region API
 chrome.runtime.onInstalled.addListener(function () {
 	processList.push([typeInitialize]);
-	loop();
 });
 chrome.browserAction.onClicked.addListener(function () {
 	processList.push([typeInitialize]);
